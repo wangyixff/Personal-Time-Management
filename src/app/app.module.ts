dfba625d8f4ce0ecdbd5434b42af682434ae7c3d
jamesfade <jamesfade_1234@hotmail.com>
2017-03-31 23:22:55 -0600

    Made circle work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,6 +15,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     HttpModule,
+    RouterModule,
     AppRoutingModule
   ],
   providers: [],
