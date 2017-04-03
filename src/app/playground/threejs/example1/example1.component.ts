b5ad74435b8c8b10af87c97d3b33b219b52e16ac
jamesfade <jamesfade_1234@hotmail.com>
2017-04-02 20:52:56 -0600

    Added first example using Three.js

--- src/app/playground/threejs/example1/example1.component.ts
+++ src/app/playground/threejs/example1/example1.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import THREE from 'assets/js/three.min.js';
+import { Component, OnInit, ViewChild, ElementRef, NgZone } from '@angular/core';
+import * as THREE from 'three';
 
 @Component({
   selector: 'app-example1',
@@ -10,44 +10,48 @@ export class Example1Component implements OnInit {
 
   @ViewChild("myCanvas") myCanvas: ElementRef;
 
-  constructor() { }
+  renderer: THREE.Renderer;
+  camera: THREE.Camera;
+
+  constructor(
+    private ngZone: NgZone
+  ) { }
 
   ngOnInit() {
-    let scene = new THREE.Scene();
+    this.ngZone.runOutsideAngular(() => this.animate);
+    this.init(this.myCanvas.nativeElement);
   }
 
-}
+  // draw
+  animate = () => {
 
-var scene, camera, renderer;
-var geometry, material, mesh;
+  }
 
-function init() {
- 
-    scene = new THREE.Scene();
- 
-    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
-    camera.position.z = 1000;
- 
-    geometry = new THREE.BoxGeometry( 200, 200, 200 );
-    material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
- 
-    mesh = new THREE.Mesh( geometry, material );
-    scene.add( mesh );
- 
-    renderer = new THREE.WebGLRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight );
- 
-    document.body.appendChild( renderer.domElement );
- 
-}
- 
-function animate() {
- 
-    requestAnimationFrame( animate );
- 
-    mesh.rotation.x += 0.01;
-    mesh.rotation.y += 0.02;
- 
-    renderer.render( scene, camera );
- 
+  init(canvas) {
+    this.renderer = new THREE.WebGLRenderer({ canvas: canvas });
+    this.renderer.setSize(canvas.width, canvas.height);
+
+    let camera = new THREE.PerspectiveCamera(45, canvas.width / canvas.height, 1, 500);
+    camera.position.set(0, 0, 100);
+    camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+    let scene = new THREE.Scene();
+
+    // create circle
+    let geometry = new THREE.CircleBufferGeometry( 5, 32, Math.PI/2, Math.PI );
+    let material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+    let circle = new THREE.Mesh( geometry, material );
+    scene.add(circle);
+    
+    // create circle
+    let geometry2 = new THREE.CircleBufferGeometry( 5, 32, 0, Math.PI/2 );
+    let material2 = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    let circle2 = new THREE.Mesh( geometry2, material2 );
+    scene.add(circle2);
+    
+
+
+    this.renderer.render(scene, camera);
+  }
+
 }
