d97f9ba9dbbee5b98bb70ff1a201ff5de8e37a18
jamesfade <jamesfade_1234@hotmail.com>
2017-04-05 22:54:10 -0600

    finished drag along x axis

--- src/app/playground/threejs/example1/example1.component.ts
+++ src/app/playground/threejs/example1/example1.component.ts
@@ -28,6 +28,16 @@ export class Example1Component implements OnInit {
   raycaster: THREE.Raycaster;
   mouse: { x: number, y: number } = {x: 1, y: 1};
 
+  // camera rotation
+  angle: number = 0;
+
+  // flags
+  selectFlag: boolean = false;
+  mouseDownFlag: boolean = false;
+  mouseMoveFlag: boolean = false;
+  mouseUpFlag: boolean = false;
+  dragFlag: boolean = false;
+
   constructor(
     private ngZone: NgZone,
     private router: Router
@@ -40,29 +50,22 @@ export class Example1Component implements OnInit {
 
   // draw
   animate = () => {
-    // update the picking ray with the camera and mouse position
-    this.raycaster.setFromCamera(this.mouse, this.camera);
-
     // calculate objects intersecting the picking ray
-    var intersects: any = this.raycaster.intersectObjects(this.scene.children);
+    this.select();
 
-    for (var i = 0; i < intersects.length; i++) {console.log("found");console.log(this.scene.children);
-      intersects[i].object.material.color.set(0xff0000);console.log("intersects:", intersects);console.log("mouse: ", this.mouse.x, this.mouse.y);
-      this.router.navigate(intersects[i].object.url);
-    }
+    // drag
+    this.drag();
 
-    this.mouse.x = 1;
-    this.mouse.y = 1;
     
     this.renderer.render(this.scene, this.camera);
 
     requestAnimationFrame( this.animate );
     
-    this.j=(this.j+0.2)%360;
-    let rad = (this.j) * Math.PI / 180;
-    this.camera.position.set(0, 100 * Math.sin(rad), 100 * Math.cos(rad));
-    //this.camera.lookAt(new THREE.Vector3(0, 0, 0));
-    this.camera.rotation.x=-this.j/180*Math.PI;
+    // this.j=(this.j+0.2)%360;
+    // let rad = (this.j) * Math.PI / 180;
+    // this.camera.position.set(0, 100 * Math.sin(rad), 100 * Math.cos(rad));
+    // //this.camera.lookAt(new THREE.Vector3(0, 0, 0));
+    // this.camera.rotation.x=-this.j/180*Math.PI;
 
     //this.camera.rotateY(this.j);
     //this.camera.rotateZ(this.j*100);
@@ -104,31 +107,78 @@ export class Example1Component implements OnInit {
 
   }
 
-  onMouseClick($event) {
+  onMouseClick($event: MouseEvent) {
     // calculate mouse position in normalized device coordinates
     // (-1 to +1) for both components 
     this.mouse.x = ($event.clientX / this.myCanvas.nativeElement.width) * 2 - 1;
     this.mouse.y = - ($event.clientY / this.myCanvas.nativeElement.height) * 2 + 1;
+    // this.selectFlag = true;
   }
 
 
+  mouseDown: {x: number, y: number} = {x: 0, y: 0};
+  onMouseDown($event: MouseEvent) {
+    // console.log($event, $event.clientY);
+    // get coordinates
+    this.mouseDown.x = $event.clientX;
+    this.mouseDown.y = $event.clientY;
+    // set flag
+    this.mouseDownFlag = true;
+  }
 
-  onMouseDown($event) {
-    // will stop any other event from firing such as the mouse's Trackball controls   
-    //event.preventDefault();
-    this.mouse.x = ($event.clientX / this.myCanvas.nativeElement.width) * 2 - 1;
-    this.mouse.y = - ($event.clientY / this.myCanvas.nativeElement.height) * 2 + 1;
-    var vector = new THREE.Vector3(
-        ( $event.clientX / window.innerWidth ) * 2 - 1,
-      - ( $event.clientY / window.innerHeight ) * 2 + 1,0.5);
-    this.projector.unprojectVector( vector, this.camera );
+  mouseMove: {x: number, y: number} = {x: 0, y: 0};
+  onMouseMove($event: MouseEvent){
+    // console.log($event.clientX);
+    // get coordinates
+    this.mouseMove.x = $event.clientX;
+    this.mouseMove.y = $event.clientY;
+    // dragFlag
+    if(this.mouseDownFlag){
+      this.dragFlag = true;
+    }
+  }
 
-    var ray = new THREE.Ray( camera.position, 
-                             vector.subSelf( camera.position ).normalize() );
+  onMouseUp($event: MouseEvent){
+    // reset mouseDownFlag
+    this.mouseDownFlag = false;
+    // set mouseUpFlag
+    this.mouseUpFlag = true;
+  }
 
+  select(){
+    if(this.selectFlag){
+      // update the picking ray with the camera and mouse position
+      this.raycaster.setFromCamera(this.mouse, this.camera);
 
-  } 
+      let intersects: any = this.raycaster.intersectObjects(this.scene.children);
+      for (let i = 0; i < intersects.length; i++) {
+        intersects[i].object.material.color.set(0xff0000);
+        this.router.navigate(intersects[i].object.url);
+      }
 
+      this.selectFlag = false;
+    }
+  }
+
+  drag(){
+    // check flag
+    if(this.dragFlag){
+      let angleUnit = Math.PI / 600 ;
+      let distance = Math.sqrt(Math.pow(this.mouseDown.x - this.mouseMove.x, 2) + Math.pow(this.mouseDown.y - this.mouseMove.y, 2));
+      let direction = this.mouseMove.x - this.mouseDown.x > 0 ? 1 : -1;
+      let deltaAngle = angleUnit * distance * direction;
+      this.camera.position.set(100*Math.sin(this.angle + deltaAngle), 0, 100*Math.cos(this.angle + deltaAngle));
+      this.camera.rotation.y = (this.angle + deltaAngle);
+      // this.camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+      if(this.mouseUpFlag){
+        // store angle
+        this.angle = this.angle + deltaAngle;
+        this.mouseUpFlag = false;
+        this.dragFlag = false;
+      }
+    }
+  }
 
 
 }
