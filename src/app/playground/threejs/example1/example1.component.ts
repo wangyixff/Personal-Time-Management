00cdc03d8b051a40b53ad75b99bfc7902434dc42
jamesfade <jamesfade_1234@hotmail.com>
2017-04-08 19:21:59 -0600

    Try another method: same issue

--- src/app/playground/threejs/example1/example1.component.ts
+++ src/app/playground/threejs/example1/example1.component.ts
@@ -138,11 +138,12 @@ export class Example1Component implements OnInit {
   }
 
   mouseMove: { x: number, y: number } = { x: 0, y: 0 };
+  previousMousePosition: {x: number, y: number} = {x: 0, y: 0};
   onMouseMove($event: MouseEvent) {
     // console.log($event.clientX);
     // get coordinates
     this.mouseMove.x = $event.clientX;
-    this.mouseMove.y = $event.clientY;console.log($event);
+    this.mouseMove.y = $event.clientY;
     // dragFlag
     if (this.mouseDownFlag) {
       this.dragFlag = true;
@@ -171,38 +172,62 @@ export class Example1Component implements OnInit {
     }
   }
 
+  toRadians(distance){
+    return Math.PI * distance / 600;
+  }
+
   drag() {
     // check flag
     if (this.dragFlag) {
-      let angleUnit = Math.PI / 50;
-      let distance = Math.sqrt(Math.pow(this.mouseDown.x - this.mouseMove.x, 2) + Math.pow(this.mouseDown.y - this.mouseMove.y, 2));
-      let direction = this.mouseMove.x - this.mouseDown.x > 0 ? 1 : 1;
-      let deltaAngle = angleUnit * distance * direction;
-      // this.camera.position.set(100*Math.sin(this.angle + deltaAngle), 0, 100*Math.cos(this.angle + deltaAngle));
-      // this.camera.rotation.y = (this.angle + deltaAngle);
-      // this.camera.rotation.x=(100*Math.sin(this.angle + deltaAngle))
+      let deltaMove = {
+        x: this.mouseMove.x - this.mouseDown.x,
+        y: this.mouseMove.y - this.mouseDown.y
+      };
+
+      let deltaRotationQuaternion = new THREE.Quaternion()
+        .setFromEuler(
+          new THREE.Euler(
+            this.toRadians(deltaMove.y * 1),
+            this.toRadians(deltaMove.x * 1),
+            0,
+            'XYZ'
+          )
+        );
 
       let children = this.scene.children;
-      var axis = new THREE.Vector3((this.mouseMove.y - this.mouseDown.y), (this.mouseMove.x - this.mouseDown.x), 0);
-      let deltaMatrix, rotatedMatrix, cloneMatrix;
-      if (distance > 0) {
-        deltaMatrix = new THREE.Matrix4();
-        deltaMatrix.makeRotationAxis(axis.normalize(), deltaAngle);
-        rotatedMatrix = new THREE.Matrix4();
-        children.forEach((mesh: MyCircle) => {
-          // mesh.rotation.setFromRotationMatrix(rotatedMatrix);
-          cloneMatrix = mesh.preMatrix.clone();
-          rotatedMatrix = cloneMatrix.multiply(deltaMatrix);
-          mesh.rotation.setFromRotationMatrix(deltaMatrix);
-        });
-      }
+      children.forEach((mesh: MyCircle) => {
+        mesh.quaternion.multiplyQuaternions(deltaRotationQuaternion, mesh.quaternion);
+      });
+
+      // let angleUnit = Math.PI / 50;
+      // let distance = Math.sqrt(Math.pow(this.mouseDown.x - this.mouseMove.x, 2) + Math.pow(this.mouseDown.y - this.mouseMove.y, 2));
+      // let direction = this.mouseMove.x - this.mouseDown.x > 0 ? 1 : 1;
+      // let deltaAngle = angleUnit * distance * direction;
+      // // this.camera.position.set(100*Math.sin(this.angle + deltaAngle), 0, 100*Math.cos(this.angle + deltaAngle));
+      // // this.camera.rotation.y = (this.angle + deltaAngle);
+      // // this.camera.rotation.x=(100*Math.sin(this.angle + deltaAngle))
+
+      // let children = this.scene.children;
+      // var axis = new THREE.Vector3((this.mouseMove.y - this.mouseDown.y), (this.mouseMove.x - this.mouseDown.x), 0);
+      // let deltaMatrix, rotatedMatrix, cloneMatrix;
+      // if (distance > 0) {
+      //   deltaMatrix = new THREE.Matrix4();
+      //   deltaMatrix.makeRotationAxis(axis.normalize(), deltaAngle);
+      //   rotatedMatrix = new THREE.Matrix4();
+      //   children.forEach((mesh: MyCircle) => {
+      //     // mesh.rotation.setFromRotationMatrix(rotatedMatrix);
+      //     cloneMatrix = mesh.preMatrix.clone();
+      //     rotatedMatrix = cloneMatrix.multiply(deltaMatrix);
+      //     mesh.rotation.setFromRotationMatrix(deltaMatrix);
+      //   });
+      // }
 
 
       if (this.mouseUpFlag) {
         // store angle
         children.forEach((mesh: MyCircle) => {
           // mesh.matrix = mesh.preMatrix.multiply(deltaMatrix);
-          mesh.rotateOnAxis(axis.normalize(), deltaAngle);
+          // mesh.rotateOnAxis(axis.normalize(), deltaAngle);
         });console.log("update prematrix");
         this.mouseUpFlag = false;
         this.dragFlag = false;
